9faa7cc2ab485e584a197ed7f8b7fd471f424e7c
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Python | SAS | R <sauldata@tutanota.com>
2020-10-26 20:09:08 +1100

    Update resnet50_imageClassification.py

--- ResNet/resnet50_imageClassification.py
+++ ResNet/resnet50_imageClassification.py
@@ -2,7 +2,7 @@
 import numpy as np
 import os
 import time
-from resnet50_sy import ResNet50
+from resnet50_sy import ResNet50 # ResNet50 model
 from keras.preprocessing import image
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout,Activation,Flatten
 
